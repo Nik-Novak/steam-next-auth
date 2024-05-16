a8bbc6d72b55d96e5e02ed4213ca4f0f65f04d6f
Nik <novakn@mcmaster.ca>
2024-05-16 00:09:43 -0400

    types

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,8 @@
+
+import { SteamProfile, SteamProviderOptions } from './steam';
+import { NextRequest } from 'next/server';
+import { NextApiRequest } from 'next';
+import { OAuthConfig } from 'next-auth/providers';
+declare module 'steam-next-auth' {
+  export default function SteamProvider(req: Request | NextRequest | NextApiRequest, options: SteamProviderOptions): OAuthConfig<SteamProfile>;
+}
